907c42ca4f7ddc44fe6951e67e04a18800a88430
liangchen08330112 <3366211746@qq.com>
2022-05-09 18:02:15 +0800

    2022.05.09 18:02梁宸提交。

--- com/sict/self-learning/demo_ellipse.py
+++ com/sict/self-learning/demo_ellipse.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = np.zeros((200,320,3),np.uint8)+255                 #创建白色图像
+img = np.zeros((200,320,3),np.uint8)+255                 #创建一个白色图像
 cv2.ellipse(img,(160,100),(120,50),0,0,360,(255,0,0),5)  #画椭圆形蓝色边框
 cv2.ellipse(img,(160,100),(60,15),0,0,360,(0,255,0),51)  #画绿色椭圆
 cv2.imshow('ellipse',img)
--- com/sict/self-learning/demo_polylines.py
+++ com/sict/self-learning/demo_polylines.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-img=np.zeros((200,320,3),np.uint8)
-pts=np.array([[160,20],[20,100],[160,180],[300,100]],np.int32)
-cv2.polylines(img,[pts],True,(255,0,0),5)
+img=np.zeros((200,320,3),np.uint8)                             #创建一幅黑色图像
+pts=np.array([[160,20],[20,100],[160,180],[300,100]],np.int32) #创建顶点
+cv2.polylines(img,[pts],True,(255,0,0),5)                      #画蓝色边框多边形
 cv2.imshow('polylines',img)
 cv2.waitKey()
 cv2.destroyAllWindows()
